be0399158e802e9e87abfba0f45559b38ede898b
TestGoing <3054105120@qq.com>
2018-09-12 17:50:30 +0800

    增加0706.cpp -- 使用正确初始化的常量变量

---
+++ dir7/0706.cpp
@@ -0,0 +1,12 @@
+// Using a properly initialized constant variable.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int x = 7; // initialized constant variable
+
+	cout << "The value of constant variable x is: " << x << endl;
+}
+
+//g++ 0706.cpp -o 0706 -std=gnu++11
