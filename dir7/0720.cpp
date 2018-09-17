c7c0a4f1ded95a02ecd3c8b6b31a41f962aba8f2
TestGoing <3054105120@qq.com>
2018-09-17 21:21:54 +0800

    增加0720.cpp -- 多维array对象的初始化

---
+++ dir7/0720.cpp
@@ -0,0 +1,32 @@
+// Initializing multidimensional arrays
+#include <iostream>
+#include <array>
+using namespace std;
+
+const size_t rows = 2;
+const size_t columns = 3;
+
+void printArray( const array<array<int, columns>, rows> & );
+
+int main()
+{
+	array< array< int, columns >, rows> array1 = {1, 2, 3, 4, 5, 6};
+	array< array< int, columns >, rows> array2 = {1, 2, 3, 4, 5};
+
+	cout << "Values in array1 by row are:" << endl;
+	printArray( array1 );
+
+	cout << "\nValues in array2 by row are:" << endl;
+	printArray( array2 );
+}
+
+void printArray( const array< array< int, columns>, rows> & a )
+{
+	for( auto const & row : a ) // auto 关键字告诉编译器根据这个变量的初始化值来确定它的数据类型
+	{
+		for( auto const & element : row )
+			cout << element << ' ';
+
+		cout << endl;
+	}
+}
