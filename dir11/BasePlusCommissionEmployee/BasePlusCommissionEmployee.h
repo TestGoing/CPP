2429d24a61f04963e6a16f802348b9075161ba16
TestGoing <3054105120@qq.com>
2018-09-30 17:38:05 +0800

    add CommissionEmployee dir and BasePlusCommissionEmployee dir

---
+++ dir11/BasePlusCommissionEmployee/BasePlusCommissionEmployee.h
@@ -0,0 +1,42 @@
+//BasePlusCommissionEmployee class definition represents an employee
+//that receive a base salary in addition to commission.
+#ifndef BASEPLUS_H
+#define BASEPLUS_H
+
+#include <string>
+
+class BasePlusCommissionEmployee
+{
+public:
+	BasePlusCommissionEmployee( const std::string &, const std::string &,
+	                            const std::string &, double = 0.0, double = 0.0, double = 0.0 );
+	void setFirstName(const std::string &);
+	std::string getFirstName() const;
+
+	void setLastName(const std::string &);
+	std::string getLastName() const;
+
+	void setSocialSecurityNumber (const std::string &);
+	std::string getSocialSecurityNumber() const;
+
+	void setGrossSales(double);
+	double getGrossSales() const;
+
+	void setCommissionRate(double);
+	double getCommissionRate() const;
+
+	void setBaseSalary(double);
+	double getBaseSalary() const;
+
+	double earnings() const;
+	void print() const;
+private:
+	std::string firstName;
+	std::string lastName;
+	std::string socialSecurityNumber;
+	double grossSales;
+	double commissionRate;
+	double baseSalary;
+};
+
+#endif
