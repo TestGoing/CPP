b66712d884b906d991121205a0d8d4bfc4d99d39
TestGoing <3054105120@qq.com>
2018-09-30 19:01:44 +0800

    增加GenerationCommissionEmployee目录--继承关系的实际应用，及派生类可访问的protected基类数据

---
+++ dir11/GenerationCommissionEmployee/BasePlusCommissionEmployee.cpp
@@ -0,0 +1,47 @@
+//Class BasePlusCommissionEmployee member-function definition
+
+#include <iostream>
+#include <stdexcept>
+#include "BasePlusCommissionEmployee.h"
+
+using namespace std;
+
+BasePlusCommissionEmployee::BasePlusCommissionEmployee(
+   const string & first, const string & last, const string & ssn,
+   double sales, double rate, double salary )
+	: CommissionEmployee( first, last, ssn, sales, rate ) //explicitly call base-class constructor
+{
+	setBaseSalary( salary ); //validate and store base salary
+}
+
+void BasePlusCommissionEmployee::setBaseSalary( double salary )
+{
+	if( salary >= 0.0 )
+		baseSalary = salary;
+	else
+		throw invalid_argument( "Salary must be >= 0.0" );
+}
+
+double BasePlusCommissionEmployee::getBaseSalary()const
+{
+	return baseSalary;
+}
+
+double BasePlusCommissionEmployee::earnings() const
+{
+	return baseSalary + ( commissionRate * grossSales );
+}
+
+void BasePlusCommissionEmployee::print() const
+{
+	//derived class cannot access the base class'private data
+	cout << "base-salaried commission employee: " << firstName << ' '
+	     << lastName << "\nsocia security number: " << socialSecurityNumber
+	     << "\ngross sales: " << grossSales
+	     << "\ncommission rate: " << commissionRate
+	     << "\nbase salary: " << baseSalary;
+}
+
+
+
+
