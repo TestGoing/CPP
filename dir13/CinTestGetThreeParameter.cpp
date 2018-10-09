401255fd99d9a548f5f3ff208b456bc05ea99a24
TestGoing <3054105120@qq.com>
2018-10-09 17:43:16 +0800

    cin.get(array,size,control)遇到控制字符结束，控制字符还在输入流中

--- dir13/CinTestGetThreeParameter.cpp
+++ dir13/CinTestGetThreeParameter.cpp
@@ -1,6 +1,6 @@
 /*
 用法：cin.get(arrayname,size,s)  把数据输入到arrayname字符数组中，当到达长度size时结束或者遇到字符s时结束
-注释：a必须是字符数组，即char a[]l类型，不可为string类型；size为最大的输入长度；s为控制,s还在缓冲区中
+注释：a必须是字符数组，即char a[]l类型，不可为string类型；size为最大的输入长度；s为控制,s还在缓冲区中(输入流)
 */
 
 
