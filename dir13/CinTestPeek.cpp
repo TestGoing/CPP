1d56727438bd53e75f3f66299c2e22af0537daae
TestGoing <3054105120@qq.com>
2018-10-09 18:23:58 +0800

    istream的成员函数peek

---
+++ dir13/CinTestPeek.cpp
@@ -0,0 +1,33 @@
+//istream::peek example
+#include <iostream> //std::cin,std::cout
+#include <string> //std::string
+#include <cctype> //std::isdigit
+#include <cstdio> // EOF
+
+using namespace std;
+
+int main()
+{
+	cout << "Please, enter a number or a word:";
+	cout.flush();
+
+	cin >> std::ws; // eat up any leading white spaces
+	int c = cin.peek();
+	if( c == EOF )
+	{
+		return 1;
+	}
+	if( isdigit( c ) )
+	{
+		int n;
+		cin >> n;
+		cout << "You entered the number: " << n << '\n';
+	}
+	else
+	{
+		string str;
+		cin >> str;
+		cout << "You entered the word:" << str << '\n';
+	}
+	return 0;
+}
