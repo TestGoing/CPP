b1f7d9c8e41c75704de0ddb706cb92956e6eeaf8
TestGoing <3054105120@qq.com>
2018-11-29 11:03:29 +0800

    函数基础

---
+++ PrimerC++/6/6_8_9/fact.cpp
@@ -0,0 +1,14 @@
+#include "test.h"
+#include <iostream>
+
+using namespace std;
+
+int fact(int val)
+{
+	if(val < 0)
+		return -1;
+	int ret = 1;
+	for(int i = 1; i != val + 1; ++i)
+		ret *= i;
+	return ret;
+}
---
+++ PrimerC++/6/6_8_9/test.h
@@ -0,0 +1,8 @@
+#ifndef TEST_H
+#define TEST_H
+
+int fact(int);
+double myABS(double);
+double myABS2(double);
+
+#endif
